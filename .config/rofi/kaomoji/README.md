9f0f580ce6e9641521c515a2151f8298498414ea
heyrict <xiezh0831@yahoo.co.jp>
2022-05-30 08:45:27 +0800

    Update: 2022-05-30

--- .config/rofi/kaomoji/README.md
+++
@@ -1,5 +0,0 @@
-# Kaomoji Rofi
-
-This is a clone of https://gitlab.com/ceda_ei/kaomoji-rofi modified by me <heyrict@yahoo.co.jp> which directly inputs the kaomoji without playing with the clipboard.
-
-This copy is licensed under GPLv3
--- .config/rofi/kaomoji/kaomoji.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-current_wid=$(xdotool getactivewindow)
-selection=$(rofi -i -dmenu -p kaomoji $@ < $(dirname $0)/kaomoji.txt)
-kaomoji=$(echo $selection | sed "s/\t.*//")
-xdotool type --window $current_wid --clearmodifiers "$kaomoji"
